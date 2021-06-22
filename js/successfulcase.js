eec212c4413fe41e88ca3892b03847fdc2c5d59d
liujiaxin-123 <3161476162@qq.com>
2021-06-22 15:50:55 +0800

    6.22
    1

--- js/successfulcase.js
+++ js/successfulcase.js
@@ -4,9 +4,12 @@
 				url:'http://192.168.8.103/item2/Hd-OutsourcingWebsite/php/',
 				imgUrl:'http://192.168.8.103/item2/Hd-OutsourcingWebsite/php/images/',
 				ifyprojects:[],
-				ifyprojects_item:[]
+				ifyprojects_item:[],
+				bannerImgs: [],
+					banner_title: [],
 			},
 			methods: {
+				
 				ifyproject(){
 					axios.get(this.url+'ifyproject.php').then(res=>{
 						// console.log(res)
@@ -17,6 +20,14 @@
 						})
 						this.ifyprojects[0].active=true
 					})
+					// 轮播
+					let ban = this.url + "/banner.php"
+					axios.get(ban).then(res => {
+						console.log(res.data)
+						this.bannerImgs = res.data.bannerImgs
+						this.banner_title = res.data.banner_title
+						// console.log(this.banner_title)
+					})
 				},
 				// 点击切换
 				ifyprojectBtn(index){
@@ -25,7 +36,14 @@
 						val['active']=false
 					})
 					this.ifyprojects[index].active=true
-				}
+				},
+				// 跳转app页面
+				fun(index) {
+					// console.log(index)
+					if (this.banner_title[index] == 'APP开发') {
+						window,open('app.html')
+					}
+				},
 			},
 			mounted() {
 				this.ifyproject()
