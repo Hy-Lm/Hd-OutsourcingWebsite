b5de9e938f43226a2d9bef7785af686245eb1126
yefei-yefei <80298453+yefei-yefei@users.noreply.github.com>
2021-06-23 17:17:13 +0800

    6-23 17：16
    php上传

---
+++ php/addTitle.php
@@ -0,0 +1,22 @@
+<?php
+// 连接数据库
+include('./public.php');
+// 接收的参数
+$title=$_GET['title'];//节目内容
+		//--解析Json，获取对应的变量值
+		$titles=json_decode($title,TRUE);
+		$mesTitle = $titles;
+		$con=null;           //存储内容
+		foreach($mesTitle as $val){
+			$con.=$val.',';
+		}
+		// 得到的标题整个数组字符串
+		$con= substr($con,0,strlen($con)-1);
+		$sql="update banner set banner_title='$con'";
+		$res=$conn->query($sql);
+		if($res){
+			echo 'ok';
+		}else{
+			echo 'no';
+		}
+?>
