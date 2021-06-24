a33a7d065807b4327780dd6d9908720167425808
yefei-yefei <80298453+yefei-yefei@users.noreply.github.com>
2021-06-24 14:12:20 +0800

    6-24 14：12
    php修改

--- php/public.php
+++ php/public.php
@@ -1,5 +1,5 @@
 <?php
-    // $conn=new mysqli('localhost','r/oot','','hdwb');
+    // $conn=new mysqli('localhost','root','','hdwb');
 	$conn=new mysqli('localhost','root','x4AFdY3ki4f5rCJA','huidait');
     $conn->query('set names utf8');
     header('Access-Control-Allow-Origin:*');
