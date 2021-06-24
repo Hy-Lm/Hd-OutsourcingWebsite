006e1be1b632322b95d92bb738dfdc6864e5a37c
yefei-yefei <80298453+yefei-yefei@users.noreply.github.com>
2021-06-24 20:24:18 +0800

    6-24
    public

--- php/public.php
+++ php/public.php
@@ -3,6 +3,15 @@
 	$conn=new mysqli('localhost','root','x4AFdY3ki4f5rCJA','huidait');
     $conn->query('set names utf8');
     header('Access-Control-Allow-Origin:*');
+	if (!function_exists('array_column')) {
+	 function array_column($arr2, $column_key) {
+	  $data = [];
+	  foreach ($arr2 as $key => $value) {
+	   $data[] = $value[$column_key];
+	  }
+	  return $data;
+	 }
+	}
 	$URL="./php/images";
 	// $URL='http://192.168.7.108/item2/Hd-OutsourcingWebsite/php/images/';
 ?>
