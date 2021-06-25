0f3443c041448c2ef63242ec0a61ec0c5a02243f
liujiaxin-123 <3161476162@qq.com>
2021-06-25 18:40:33 +0800

    6.25
    6.25

--- js/successfulcase.js
+++ js/successfulcase.js
@@ -2,7 +2,7 @@
 			el: '#app',
 			data: {
 				// url: 'http://192.168.7.108/item2/Hd-OutsourcingWebsite/php/',
-				url:'http://www.huidait.cn/php',
+				url:'http://www.huidait.cn/php/',
 				imgUrl:'http://www.huidait.cn/php/images/',
 				ifyprojects:[],
 				ifyprojects_item:[],
