dfbc44b00097e95605a045f2cc5d1217e509dee3
song-xuebo <13663665247@163.com>
2021-07-08 16:43:17 +0800

    百度监听
    百度监听

---
+++ js/huidait.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a53dc832263262b3d3a062356e57d3f9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
